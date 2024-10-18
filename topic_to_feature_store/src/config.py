4157017ab9ff6cac9523fb6995492e4125c0c04c
Franciszek Migaszewski <fmigaszewski@gmail.com>
2024-10-18 14:49:25 +0000

    Edited 'config.py'

--- topic_to_feature_store/src/config.py
+++ topic_to_feature_store/src/config.py
@@ -6,7 +6,7 @@ from typing import List, Optional
 class AppConfig(BaseSettings):
     kafka_broker_address: Optional[str] = None
     kafka_input_topic: str
-    kafka_consumer_group: Optional[str] = None
+    kafka_consumer_group: str
 
     feature_group_name: str
     feature_group_version: int
